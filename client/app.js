a8f97b9588268c54ae4ac4205de4d3cf5f834a7f
zhyan5726 <zhyan5726@fiberhome.com>
2017-10-24 14:59:41 +0800

    [A]微信小程序天气预报

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 /**
  * @fileOverview 微信小程序的入口文件
  */
-
+//app.js
 var qcloud = require('./vendor/wafer2-client-sdk/index');
 var config = require('./config');
 
@@ -12,4 +12,19 @@ App({
     onLaunch() {
         qcloud.setLoginUrl(config.service.loginUrl);
     }
+    
+    //1、系统事件部分
+    onLaunch:function(){//小程序初始化时执行
+    	var that=this;
+    	that.curid=wx.getStorageSync('curid')||that.curid;//API：获取本地缓存，若不存在设置为全局属性
+    	that.setlocal('curid',that.curid);//调用全局方法
+    },
+    //2、自定义全局方法部分
+    setlocal:function(id,val){
+    	wx.setStorageSync(id,val);//API:设置本地缓存
+    },
+    //3、自定义全局属性部分
+    curid:"CN101010100",
+    version:"1.0"
+    
 });
